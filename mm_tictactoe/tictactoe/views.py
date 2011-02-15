49706559ab90c1771f239af343f5c63b836e18c6
Michael Matthews <*@*>
2011-02-15 04:55:40 -0500

    basic "login" and gameplay views and templates.  Working game.  I can't beat it, but it doesn't know it yet (still need winner/loser handling)

--- mm_tictactoe/tictactoe/views.py
+++ mm_tictactoe/tictactoe/views.py
@@ -1,18 +1,56 @@
 # Create your views here.
+from datetime import datetime
 
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
 from django.shortcuts import render_to_response
-from django.views.generic.simple import direct_to_template
+
+from models import Player, Game
 
 def home(request):
-    print "boo!"
-    return render_to_response('home.html')
+    return render_to_response('home.html',
+            context_instance=RequestContext(request))
 
 def login(request):
-    pass
+    playerName = request.POST.get("playerName")
+    if not playerName:
+        return None # TODO: Throw an exception
 
-def setUserAndStart(request):
-    pass
+    player = Player.objects.get_or_create(name=playerName)[0]
+    player.lastLogin = datetime.now()
+
+    game = Game(player1=player)
+    game.nextTurn()
+
+    player.save()
+    game.save()
+    request.session['player'] = player
+    request.session['game'] = game
+    return HttpResponseRedirect('/game/')
 
 def game(request):
-    pass
+    game = request.session['game']
+    player = request.session['player']
+
+    if request.method == 'POST':
+        index = int(request.POST['index'])
+        playerChar = game.O
+        otherPlayerChar = game.X
+        if player == game.player2:
+            playerChar = game.X
+            otherPlayerChar = game.O
+
+        game.board[index] = playerChar
+        if game.checkWinner(playerChar):
+            # TODO: Handle winning
+            pass
+
+        if game.nextTurn(otherPlayerChar):
+            # TODO: Handle losing
+            pass
+
+        game.save()
+        request.session['game'] = game
+    return render_to_response('game.html', {'game': game },
+            context_instance=RequestContext(request))
+
