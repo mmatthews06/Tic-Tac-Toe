044147a9540cf60883e944f55e5e3c220c826331
Michael Matthews <*@*>
2011-02-17 02:33:10 -0500

    just a couple more tests because I thought I spotted a bug.  The tests still are not comprehensive.  Also, center the game board.

--- mm_tictactoe/tictactoe/tests.py
+++ mm_tictactoe/tictactoe/tests.py
@@ -27,22 +27,84 @@ class TicTacToeTest(TestCase):
 
     def test_3GamesToDraw(self):
         """
+        Simply play three games with computer X and O moves, and make
+        sure nobody wins.
         """
         game1 = self.__playGame()
-        print "Board:\n", game1
         self.assertEqual(filter(lambda x: x==0, game1.board), [])
         self.assertFalse(game1.checkWinner(game1.X))
         self.assertFalse(game1.checkWinner(game1.O))
 
         game2 = self.__playGame()
-        print "Board:\n", game2
         self.assertEqual(filter(lambda x: x==0, game2.board), [])
         self.assertFalse(game2.checkWinner(game2.X))
         self.assertFalse(game2.checkWinner(game2.O))
 
         game3 = self.__playGame()
-        print "Board:\n", game3
         self.assertEqual(filter(lambda x: x==0, game3.board), [])
         self.assertFalse(game3.checkWinner(game3.X))
         self.assertFalse(game3.checkWinner(game3.O))
+
+
+    def test_winBeforeBlock(self):
+        """
+        The computer move should win the game before blocking.  Also,
+        the nextTurn() method should return True, since this move produced
+        a winner, and the checkWin() method should return true for X, but
+        false for O.
+
+        Starting board:
+            O O _
+            X X _
+            _ _ _
+
+            It's the X player's turn.  It should win the game as such:
+            O O _
+            X X X
+            _ _ _
+
+        """
+
+        startingBoard = [Game.O, Game.O, Game.EMPTY,
+                    Game.X, Game.X, Game.EMPTY,
+                    Game.EMPTY, Game.EMPTY, Game.EMPTY]
+
+
+        expected = [Game.O, Game.O, Game.EMPTY,
+                    Game.X, Game.X, Game.X,
+                    Game.EMPTY, Game.EMPTY, Game.EMPTY]
+
+        player1 = Player(name="player1")
+        player2 = Player(name="player2")
+        game = Game(player1=player1, player2=player2)
+        game.board = startingBoard
+        game.turn = 4
+        win = game.nextTurn(Game.X)
+        self.assertEqual(game.board, expected)
+        self.assertTrue(win)
+        self.assertTrue(game.checkWinner(game.X))
+        self.assertFalse(game.checkWinner(game.O))
+
+    def test_blockWin(self):
+        """
+        Block a potential win before a fork.
+        """
+
+        startingBoard = [Game.O, Game.EMPTY, Game.X,
+                    Game.EMPTY, Game.X, Game.EMPTY,
+                    Game.O, Game.EMPTY, Game.EMPTY]
+
+
+        expected = [Game.O, Game.EMPTY, Game.X,
+                Game.X, Game.X, Game.EMPTY,
+                Game.O, Game.EMPTY, Game.EMPTY]
+
+        player1 = Player(name="player1")
+        player2 = Player(name="player2")
+        game = Game(player1=player1, player2=player2)
+        game.board = startingBoard
+        game.turn = 4
+        game.nextTurn(Game.X)
+        self.assertEqual(game.board, expected)
+
 
