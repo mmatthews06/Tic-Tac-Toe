0a71bd1e7d2e36674b84db644f23f9351a9c4f5d
Michael Matthews <*@*>
2011-05-17 02:07:45 -0400

    "Add to home screen" bubble (from Google's library), and icons

--- mm_tictactoe/site_media/script.js
+++ mm_tictactoe/site_media/script.js
@@ -124,3 +124,23 @@ function navClicked(e) {
     }
     
 }
+
+window.addEventListener('load', function() {
+  window.setTimeout(function() {
+    var bubble = new google.bookmarkbubble.Bubble();
+
+    var parameter = 'bmb=1';
+
+    bubble.hasHashParameter = function() {
+      return window.location.hash.indexOf(parameter) != -1;
+    };
+
+    bubble.setHashParameter = function() {
+      if (!this.hasHashParameter()) {
+        window.location.hash += parameter;
+      }
+    };
+
+    bubble.showIfAllowed();
+  }, 1000);
+}, false);
